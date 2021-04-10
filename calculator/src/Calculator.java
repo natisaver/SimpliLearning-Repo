92afb713cbbc1bd350f2357c3f549c7d54c7101c
nat <64686163+natisaver@users.noreply.github.com>
2021-04-10 16:00:27 +0800

    Update Calculator.java

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,51 +1,75 @@
 import java.util.Scanner;
 
-public class Calculator {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        double num1, num2; //same type can write multiple times
-        double answer = 0.0;
-        char operator;
-        boolean status = false;
-        
-        do{
-            System.out.println("Enter First Number: ");
-            
-            System.out.println("Enter Second Number: ");
-            /*nextLine() is for strings, each primitive type got a method, 
-            double in this case uses nextDouble()
-            */
-            if(sc.hasNextDouble()){
-                num1 = sc.nextDouble(); //storing the input into num1
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
 
-                System.out.println("ur val is");
-                status = true;
-            } else{
-                System.out.println("not valid number");
-                status = false;
+public class Calculator {
+    static Scanner sc = new Scanner(System.in);
+    static double num1, num2; // same type can write multiple times
+    static double answer = 0.0;
+    static char operator;
+    static boolean valid = false; // used to continue while loop, if num return error
+    static boolean start = false;
+
+    public static void main(String[] args) {
+
+        do {
+            startcal();
+
+            System.out.println("What Operator (+, -, *, /): ");
+
+            operator = sc.next().charAt(0);
+
+            // switch statement for cases
+            switch (operator) {
+            case '+':
+                answer = num1 + num2;
+                break;
+            case '-':
+                answer = num1 - num2;
+                break;
+            case '*':
+                answer = num1 * num2;
+                break;
+            case '/':
+                answer = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid Operator");
+                break;
             }
 
-    } while (status = false);
+            System.out.println(num1 + " " + num2 + " = " + answer);
+            start = false;
+            System.out.println("you want use again? Type Y to continue");
+            char yes = sc.next().charAt(0);
+            if (yes == 'y' || yes == 'Y') {
+                start = true;
+            } else
+                System.out.println("k bye.");
 
-    System.out.println("What Operation? ");
-    operator = sc.next().charAt(0);
+        } while (start == true);
 
-    //switch statement for cases
-    switch (operator) {
-        case '+': answer = num1 + num2;
-            break;
-        case '-': answer = num1 - num2;
-            break;
-        case '*': answer = num1 * num2;
-            break;
-        case '/': answer = num1 / num2;
-            break;
-        default:
-            System.out.println("Invalid Number");
-            break;
     }
-    System.out.println(num1+" "+num2+" = "+answer);
 
-    
-}
+    // startcal
+
+    static void startcal() {
+        System.out.println("Enter First Number");
+        while (!sc.hasNextDouble()) {
+            System.out.println("Please key a valid number");
+            sc.next();
+        }
+        num1 = sc.nextDouble();
+        /*
+         * nextLine() is for strings, each primitive type got a method, double in this
+         * case uses nextDouble()
+         */
+        System.out.println("Enter second number:");
+        while (!sc.hasNextDouble()) {
+            System.out.println("Please key a valid number");
+            sc.next();
+        }
+        num2 = sc.nextDouble();
+    }
+
 }
